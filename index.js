c88a7b3e5f325d6b11c9dd54c2e0421947b2b994
Li Yi <i@ijser.cn>
2018-03-29 13:27:42 +0800

    update theme

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-import './theme/default'
+import './theme/index.scss';
+
+function loadTheme (name) {
+  $('html').attr('theme', name)
+  $('.page-nav .navbar-item.is-active').removeClass('is-active')
+  $(`.page-nav .navbar-item[data-theme=${name}`).addClass('is-active')
+}
 
 $('[data-theme]').on('click', function (event) {
   const theme = this.dataset.theme
-  import('./theme/dark').then(() => {
-    $('html').attr('theme', theme)
-  })
+  loadTheme(theme)
   return false
 })
+
+loadTheme('dark')
